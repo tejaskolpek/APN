eca3ac3df6479394d0475e1d0ed4a6d02d1f63ee
Tej Kolpek <tejkolpek@gmail.com>
2025-10-23 14:39:23 -0400

    tk

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  distDir: 'out',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Me' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/Me' : '',
-}
-
-module.exports = nextConfig
+const nextConfig = { output: 'export' };
+module.exports = nextConfig;
